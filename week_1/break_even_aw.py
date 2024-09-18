292244e9baed54bcc582b8d07bcde92097efe5fc
TuringProblem <andrew.deca.wellington@gmail.com>
2024-09-17 20:52:29 -0400

    feat: finished the second assignment :)

--- week_1/break_even_aw.py
+++ week_1/break_even_aw.py
@@ -1,6 +1,18 @@
-# BEP Units = Total Fixed Costs / (Sales Price Per Unit - Variable Cost Per Unit)
+#*******************#
+# Author: Andrew W. #
+#*******************#
+#   @09/17/2024     #
+#*******************#
+tfc = float(input("Please enter the Total Fixed Costs: "))
+sppu = float(input("Please enter the Sales Price: "))
+vcpu = float(input("Please enter the Variable "))
 
-# Contribution Margin Per Unit = Sales Price Per Unit  - Variable Cost Per Unit
+bep_units = (tfc / (sppu - vcpu))
+cmpu = sppu - vcpu
+cmrpu = cmpu / sppu
+bep_sdollars = tfc / cmrpu
 
-
-
+print(f"BEP Units: {bep_units}")
+print(f"Contribution Margin Per Unit: {cmpu}")
+print(f"Contribution Margin Ratio Per Unit: {cmrpu}")
+print(f"BEP Sales Dollars: {bep_sdollars}")
