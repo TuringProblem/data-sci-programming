b061ecc82e34c73553f8ffaaf29f1db8ddc3b4ca
Override <122590935+TuringProblem@users.noreply.github.com>
2024-10-02 21:11:56 -0400

    feat: finished all of the assignments, now need to submit to the teacher -> learned about classes, and how functions actually work within an OOP approach, I should test myself at the end of everyweek adn see what I can remember

--- week_2/programapartment_affordability.py
+++ week_2/programapartment_affordability.py
@@ -1,4 +1,30 @@
-class ProgramApartment:
+####################
+## Author: Andrew ##
+####################
+
+def main():
+    elevator = False
+    sqft = int(input("Enter the apartment size in: "));
+    bedroom = int(input("Enter the number of bedrooms: "));
+    bathroom = int(input("Enter the number of bathrooms: "));
+    flr = int(input("Enter the floor: "));
+    elevator = input("Enter elevator (true or false): ").lower();
+    offst_parking = input("Enter off street parking (true or false): ");
+    rent = int(input("Enter how much rent: "));
+
+    is_elevator = elevator == 'true';
+    is_offst = offst_parking == 'true';
 
 
+    if (sqft >= 1500 and bedroom >= 2 and bathroom >= 2 and is_elevator and is_offst and rent <= 2000):
+        print("Include the apartment under Criteria 1\n");
+    if (sqft >= 500 and bedroom == 1 and bathroom == 1 and rent <= 1000):
+        print("Include the apartment under Criteria 2\n");
+    if (sqft >= 2000 and bedroom == 3 and bathroom == 2 and flr > 4 and is_elevator and rent <= 3000):
+        print("Include the apartment under Criteria 3\n");
+    if (sqft >= 1500 and bedroom == 3 and bathroom == 2 and flr > 4 and is_elevator and is_offst and rent <= 3000):
+        print("Include the apartment under Criteria 4\n");
 
+
+if __name__ == "__main__":
+    main()
