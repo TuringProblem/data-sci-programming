5c9b9c14c0c0f585a2a6bd762b3822480d9c7042
TuringProblem <andrew.deca.wellington@gmail.com>
2024-10-13 03:34:04 +0000

    feat: started to work on the second assingment, working on adding the right definitions and making sure the code is structured properly

---
+++ week_4/delivery_function_aw.py
@@ -0,0 +1,28 @@
+####################
+## Author: Andrew ##
+##****************##
+
+# $.10 per mile from the shop to the customer location
+# 10% for orders <= $50 and 5% for orders > $50
+miles_process = lambda a: a / 100
+
+
+def take_your_order():
+    order_amount = float(input("Please enter the amount of the order: "))
+    process = get_discount(order_amount)
+    print(f"percentage: %{process:.2f}")
+    miles_from_shop = int(input("Please enter the miles: "))
+    price_miles = miles_process(miles_from_shop)
+    print(f"Amount from miles: {price_miles}")
+
+
+def get_discount(value: float) -> float:
+    if value > 50.0:
+        return 0.05
+    if value <= 50.0:
+        return 0.10
+    else:
+        return 0.00
+
+
+take_your_order()
