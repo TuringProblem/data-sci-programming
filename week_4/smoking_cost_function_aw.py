af3e09b3a3ac9e5a7599706a5adb192b8b237270
TuringProblem <andrew.deca.wellington@gmail.com>
2024-10-13 21:01:05 +0000

    feat: need to add this .txt file for my class

---
+++ week_4/smoking_cost_function_aw.py
@@ -0,0 +1,10 @@
+####################
+## Author: Andrew ##
+##****************##
+
+def pack_of_cigs(cost: float, num_packs: int, years: int, yearly_increase=0.10):
+    for i in range(1, years + 1):
+        
+ 
+
+
