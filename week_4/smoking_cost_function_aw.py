094477dfbb8ce8c2f449020c2e48bb7cee966560
TuringProblem <andrew.deca.wellington@gmail.com>
2024-10-16 14:57:24 +0000

    feat: added the context to my solution so a layman could understand what is going on in this functional expression

--- week_4/smoking_cost_function_aw.py
+++ week_4/smoking_cost_function_aw.py
@@ -2,6 +2,12 @@
 ## Author: Andrew ##
 ##****************##
 
+## I'm actually shocked how nice this turned out to be...
+## essentially I'm taking the sum of c or COST (as a const) and y or YEAR ->
+## and for each year I take the cost plus + .10 (fixed rate of cost increase) * the years
+## This (c + 0.10 * year) is in parenthesis due to the for loop within the lambda expression
+## This allows for year 1 cost would just be 1.10 -> 2 == 1.20 ... and then * 365
+
 calc_total = lambda c, y: sum((c + 0.10 * year) * 365 for year in range(y))
 ex_cost_one = 1
 y_one = 2
