5ebade92212a477d6f82f95bb1a7193b0bd2fb1f
TuringProblem <andrew.deca.wellington@gmail.com>
2024-10-16 03:09:46 +0000

    feat: finished the last assignment with a phenominal functional approach, just need to explain it and finish off the list assignments

--- week_4/smoking_cost_function_aw.py
+++ week_4/smoking_cost_function_aw.py
@@ -2,9 +2,12 @@
 ## Author: Andrew ##
 ##****************##
 
-def pack_of_cigs(cost: float, num_packs: int, years: int, yearly_increase=0.10):
-    for i in range(1, years + 1):
-        
- 
+calc_total = lambda c, y: sum((c + 0.10 * year) * 365 for year in range(y))
+ex_cost_one = 1
+y_one = 2
+ex_cost_two = 12
+y_two = 30
 
-
+total_one = calc_total(ex_cost_one, y_one)
+total_two = calc_total(ex_cost_two, y_two)
+print(total_one)
