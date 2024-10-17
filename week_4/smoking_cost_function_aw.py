2762a2d844e7b27522eb7b7be4f7bf13ed160c01
Override <122590935+TuringProblem@users.noreply.github.com>
2024-10-16 21:22:31 -0400

    feat: Finished the assignment

--- week_4/smoking_cost_function_aw.py
+++ week_4/smoking_cost_function_aw.py
@@ -8,12 +8,14 @@
 ## This (c + 0.10 * year) is in parenthesis due to the for loop within the lambda expression
 ## This allows for year 1 cost would just be 1.10 -> 2 == 1.20 ... and then * 365
 
-calc_total = lambda c, y: sum((c + 0.10 * year) * 365 for year in range(y))
+calc_total = lambda c, y, p: sum(((c * (0.10 + 1) ** year) * p) * 365 for year in range(y))
 ex_cost_one = 1
 y_one = 2
+exOnePpd = 1
+exTwoPpd = 3
 ex_cost_two = 12
 y_two = 30
 
-total_one = calc_total(ex_cost_one, y_one)
-total_two = calc_total(ex_cost_two, y_two)
-print(total_one)
+total_one = calc_total(ex_cost_one, y_one, exOnePpd)
+total_two = calc_total(ex_cost_two, y_two, exTwoPpd)
+print(f"Example one return: {total_one}\nExample two return: {total_two}")
---
+++ week_5/example_5.py
@@ -0,0 +1,12 @@
+mylist = []
+
+for num in range(10):
+    mylist.append(num);
+    print(mylist)
+
+mylist[2] = 99
+print(mylist)
+
+mylist_two = [3, 4, "Hello", 9.2]
+mylist_three = [0] * 5
+
