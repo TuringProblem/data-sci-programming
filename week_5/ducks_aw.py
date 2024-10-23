32f7049a8afaeb18320e6d4c2cdb217b6c27530e
TuringProblem <andrew.deca.wellington@gmail.com>
2024-10-23 18:11:21 +0000

    feat: finished the list examples in class and it made a lot of sense, Python is really powerful; wow!

---
+++ week_5/ducks_aw.py
@@ -0,0 +1,51 @@
+ducks = ["Jack", "Kack", "Lack", "Mack", "Nack", "Oack", "Pack", "Qack"]
+print(ducks)
+ducks.sort()
+print(ducks)
+
+ducks.append("Uack")
+print(ducks)
+
+corner = ducks.pop(5)
+print(corner)
+
+print(ducks)
+# ducks.append(corner)
+# print(ducks)
+
+
+ducks.insert(2, "Vack")
+print(ducks)
+
+ducks.sort(reverse=True)
+print(ducks)
+
+ducks.insert(2, corner)
+print(ducks)
+
+ducks.append("Xack")
+print(ducks)
+
+p = ducks.index("Uack")
+print(f"Index={p}")
+del ducks[1]
+print(f"After deleting 'Uack' = {ducks}\n")
+
+time_out = last_duck = ducks.pop()
+print(f"Deleted the last duck: {time_out}\n")
+
+del ducks[ducks.index("Vack")]
+print(f"After kicking out Vack: {ducks}\n")
+
+ducks.append(time_out)
+print(ducks)
+
+
+del ducks[ducks.index("Xack")]
+print(ducks)
+
+print(f"Lenghth of new list: {len(ducks)}\n")
+
+
+ducks.sort()
+print(ducks)
