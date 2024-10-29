e28d31c5ff8debda1b574456a4e587c6f0cc5800
TuringProblem <andrew.deca.wellington@gmail.com>
2024-10-29 20:36:55 +0000

    feat: pretty messy code so far, but almost done with the assignment; learning more about playing with matrix's using itertools as well and list comprehension *Cheifs kiss*

--- week_5/stocks_aw.py
+++ week_5/stocks_aw.py
@@ -1,3 +1,20 @@
+import itertools
+
+#############
+## Author: Andrew ##
+##****************##
+## You have a number of stocks. You have decided to determine how much money
+## you will have after you sell all of your stocks and pay the taxes.
+## The following list details your stocks. Each line details the purchase price, sales price,
+## time pf period held ('L' indicates long-term and 'S' indicates sort-term).
+
+
+## Short-term stocks are held one year or less
+## Long-term stocks are held more than one year
+## Short-term gains will be taxed at 22% -> .22
+## Long-term gains will be taxed at 15% -> .15
+
+
 stocks = [
     [15000, 11500, "S"],
     [15000, 25100, "L"],
@@ -17,11 +34,47 @@ stocks = [
 ]
 
 
+lTerm = list(itertools.filterfalse(lambda x: x[2] == "S", stocks))
+sTerm = list(itertools.filterfalse(lambda x: x[2] == "L", stocks))
+
+print(f"\n\n\nMy Long-term: {lTerm}\n\n\n\n")
+print(f"\n\n\nMy Short-term: {sTerm}\n\n\n\n")
+
+
+def handleTerms(val):
+    gainsWithLoses = []
+    for s in val:
+        dif = s[1] - s[0]
+        gainsWithLoses.append(dif)
+        print(gainsWithLoses)
+
+    t = sum(gainsWithLoses)
+
+    print(f"\n\nValue(from: {val}): {t}\n\n")
+
+    if val == lTerm:
+        newTotalWithTax = t * 0.15
+        return newTotalWithTax
+    else:
+        newTotalWithTax = t * 0.22
+        return newTotalWithTax
+
+
+print(f"\n\nShort-term: {handleTerms(sTerm)}\t\nLong-term: {handleTerms(lTerm)}\n\n")
+
+
+sNums = [n for s in sTerm for n in s[:2]]
+lNums = [n for s in lTerm for n in s[:2]]
+
+print(f"Long-term Values: {sNums}\nLong-term Values: {lNums}\n\n")
+print(f"Short-term (before calculations): {sTerm}\n\n")
+
 ## You are to calculate and print to the screen the following:
+
 ## - total long term gains/losses
 
-## - total short term gains/lossed
 
+## - total short term gains/lossed
 
 ## - Amount of tax you will pay on total long-term gains (you do not pay taxes on losses)
 
